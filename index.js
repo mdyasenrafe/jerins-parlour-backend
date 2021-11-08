1bc6a693e03ee340d9f0af0586e388d6d9d83860
Yasen30 <mdyasenrafe@gmail.com>
2021-11-08 15:21:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = ` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6mulb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// run function
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Jerin_Parlour");
+    const servicesCollection = database.collection("services");
+    app.get("/services", async (req, res) => {
+      const cursors = servicesCollection.find();
+      const result = await cursors.toArray();
+      res.send(result);
+    });
+    console.log("database connect");
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+console.log(uri);
+app.get("/", (req, res) => {
+  res.send("<h1>This is Jerin Parlour Server</h1>");
+});
+app.listen(port, () => {
+  console.log("sucessfully work by ", port);
+});
